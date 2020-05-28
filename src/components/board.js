4db5091efd8443e4fe6b94f237fb460d3d98c12a
Tejas Cousik <tcousik0@gmail.com>
2020-05-27 22:25:10 -0600

    trimming away

--- src/components/board.js
+++ src/components/board.js
@@ -1,37 +1,25 @@
 import React from "react";
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sound: null,
-    };
-  }
-
   playSound = (id) => {
-    let sound = this.props.sounds.find((sound) => {
-      return sound.id === id;
-    });
+    let sound = this.props.sounds.find((sound) => sound.id === id);
     let currentSound = new Audio(sound.soundURL);
-    this.setState({
-      sound: currentSound,
-    });
     currentSound.play();
   };
 
-  renderSounds() {
+  renderSounds = () => {
     return this.props.sounds.map((sound) => {
       return (
         <div>
           <PicWithSound
             key={sound.id}
             sound={sound}
-            playSound={this.playSound.bind(this)}
+            playSound={this.playSound}
           />
         </div>
       );
     });
-  }
+  };
 
   render() {
     return <div className="card-container">{this.renderSounds()}</div>;
